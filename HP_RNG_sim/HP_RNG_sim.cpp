bb6b40bd986c6cc7b1a31f36a20e04a40e2463d2
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-07-06 02:20:51 -0400

    Update HP_RNG_sim.cpp

--- HP_RNG_sim/HP_RNG_sim.cpp
+++ HP_RNG_sim/HP_RNG_sim.cpp
@@ -1,5 +1,6 @@
 #define WIN32_LEAN_AND_MEAN // Meme
 
+#include <Windows.h>
 #include <iostream>
 #include <thread>
 #include <string>
@@ -14,6 +15,11 @@
 
 static bool bThreadbreak = false;
 static bool bPause = false;
+
+static int i = 0;
+static int imin;
+static int imax;
+int* results;
 
 // Thread function for pausing
 void GetKBInRunner()
@@ -45,7 +51,7 @@ void GetKBInRunner()
 // UE functions. Inlined for preformance reasons
 inline float appFrand()
 {
-	return rand() / (float)RAND_MAX;
+	return i / (float)RAND_MAX;
 }
 
 inline float RandRange(float Min, float Max)
@@ -53,75 +59,73 @@ inline float RandRange(float Min, float Max)
 	return Min + (Max - Min) * appFrand();
 }
 
-int main(int argc, char** argv)
+int ParseArg(int* iout, const char* arg, const char* errmsg = (char *)nullptr, std::logic_error* chout = (std::logic_error *)nullptr)
 {
-	srand((unsigned)time(NULL)); //do this bcz UE does this
-	if (argc == 1)
-	{
-		std::cerr << "You need to pass a number as an argument that specifies how many trials to run.";
-		return -1;
-	}
-
-	unsigned long times = 0U;
 	try
 	{
-		std::string str = argv[1];
-		times = std::stoul(str, NULL, 10);
+		std::string str = arg;
+		*iout = std::stoi(str);
+		return 0;
 	}
-	catch (std::invalid_argument)
+	catch (std::logic_error ex)
 	{
-		std::cerr << "You need to pass a number as an argument that specifies how many trials to run.";
+		if (chout)
+		{
+			*chout = ex;
+		}
+		if (errmsg)
+		{
+			std::cerr << errmsg;
+		}
 		return -1;
 	}
-	catch (std::out_of_range)
-	{
-		std::cerr << "The value " << argv[1] << " exceeds the maximum value of " << ULONG_MAX << ".\n";
-		times = ULONG_MAX;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 3) {
+		std::cerr << "You must specify two arguments, a min and max to test respectively.";
+		return -1;
 	}
 
-	int gryff_start = 30;
-
-	if (argc > 2)
+	if (
+		ParseArg(&imin, argv[1], "Argument 1 is invalid. It must be an integer.") ||
+		ParseArg(&imax, argv[2], "Argument 2 is invalid. It must be an integer.")
+		)
 	{
-		try
-		{
-			std::string str = argv[2];
-			gryff_start = std::stoi(str, NULL, 10);
-		}
-		catch (std::invalid_argument)
-		{
-			std::cerr << "You need to pass a number as an argument that specifies how many house points gryffindor starts with.";
-			return -1;
-		}
-		catch (std::out_of_range)
-		{
-			std::cerr << "The value " << argv[2] << " exceeds the maximum value of " << INT_MAX << ".\n";
-			gryff_start = INT_MAX;
-		}
+		return -1;
 	}
 
-	unsigned long i = 0U;
-	std::cout << "Running " << times << " trial";
-	if (times != 1U) {
+	if (imin >= imax) {
+		std::cerr << "Argument 1 must be less than argument 2.";
+		return -1;
+	}
+
+	float fmin = (float)imin;
+	float fmax = (float)imax;
+
+	int size = imax - imin + 1;
+	results = new int[size] {};
+	//ZeroMemory(results, size * sizeof(int));
+
+	std::cout << "Running " << (RAND_MAX + 1) << " trial";
+	if (imax != 1U) {
 		std::cout << "s";
 	}
 	std::cout << ". Press \"p\" to pause.\n";
-	unsigned long good = 0U;
-	int gryff;
-	int slyth;
 
 	// Init a thread for pausing
 	std::thread pauseThread(GetKBInRunner);
 
 	auto start = std::chrono::high_resolution_clock::now();
 	// Loop until desired
-	while (i < times)
+	while (i <= RAND_MAX)
 	{
 		// Infinite loop to pause
 		if (bPause)
 		{
 			std::cout << "Execution paused. Press \"p\" to resume or \"q\" to quit.\n"
-				<< "Trials: " << i << "\nGood: " << good << "\n";
+				<< "Trials: " << i << "\n";
 			do
 			{
 				std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -135,24 +139,25 @@ int main(int argc, char** argv)
 			while (bPause);
 			std::cout << "Resuming...\n";
 		}
-
-		gryff = gryff_start;
-		slyth = (int)RandRange(40.0F, 80.0F);
-		while (gryff <= slyth)
-		{
-			gryff += (int)RandRange(1.0F, 10.0F);
-		}
-		if (gryff - slyth <= 5)
-		{
-			good++;
-		}
+		int index = (int)RandRange(fmin, fmax) - imin;
+		results[index] = results[index] + 1;
 		i++;
 	}
 	auto timeTaken = std::chrono::high_resolution_clock::now() - start;
 	long long millis = std::chrono::duration_cast<std::chrono::milliseconds>(timeTaken).count();
 	double seconds = millis / 1000.0;
-	std::cout << "Execution completed.\nTrials: " << times << "\nGood: " << good << "\n"
-		<< "Time elapsed: " << seconds << " seconds";
+	std::cout << "Execution completed.\nTrials: " << (i-1) << "\n"
+		<< "Time elapsed: " << seconds << " seconds\n";
+
+	if (results != nullptr)
+	{
+		for (int i = imin; i <= imax; i++)
+		{
+			std::cout << i << ": " << results[i - imin] << "\n";
+		}
+	}
+
+	delete[] results;
 
 	// Break the thread
 	bThreadbreak = true;
